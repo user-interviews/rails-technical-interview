ad41223a2bfb1fa577265cb5e13808cfe9708981
Bob Saris <bob@bobsaris.com>
2018-06-02 13:43:25 -0400

    Added a send email method to use for communcation.

--- app/services/emails.rb
+++ app/services/emails.rb
@@ -2,4 +2,12 @@
 
 module Emails
   EMAIL_FORMAT_REGEX = /[^@]*@[^\.]*\..*/
+
+  def send_email(to_email, template, vars = {})
+    Mandrill.send_email(
+      template,
+      to: to_email,
+      vars: vars,
+    )
+  end
 end
